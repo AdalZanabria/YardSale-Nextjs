23cac696eaeb982e188636bb47985414b6e48f38
AdalZanabria <adal.zanabria@gmail.com>
2022-03-31 22:52:52 -0600

    Primer commit con archivos de configuracion.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/AdalZanabria',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
